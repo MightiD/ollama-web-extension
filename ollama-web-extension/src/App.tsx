665706d7460df0ed5267aff3755f1b778606310c
MightiD <128378210+MightiD@users.noreply.github.com>
2025-04-03 21:16:13 +0100

    Title UI + add tailwind

---
+++ ollama-web-extension/src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className='w-[400px] h-[500px] bg-black'>
+      <div className="flex justify-center items-center pt-10">
+        <p className="text-[14px] text-white">
+          Ollama Web Extension
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default App
